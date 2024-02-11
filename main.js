f71dd087eb289121ca07e4fed8b1208885516d28
jebi2420 <jebi2420@gmail.com>
2024-02-11 13:06:20 +0900

    [add] 로직 완성

--- main.js
+++ main.js
@@ -7,4 +7,61 @@
 // 유저가 1~100 범위 밖의 숫자를 입력하면 알려준다. 기회를 깎지 않는다
 // 유저가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회를 깎지 않는다
 
+let computerNum = 0;
+let playBtn = document.getElementById("play-btn");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetBtn = document.getElementById("reset-btn")
+let chances = 10;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
 
+playBtn.addEventListener("click", play)
+resetBtn.addEventListener("click", reset)
+userInput.addEventListener("focus", resetInput)
+
+function pickRandomNum() {
+    computerNum = Math.floor(Math.random() * 10)+1;
+    console.log("정답", computerNum);
+}
+
+function play(){
+   let userValue = userInput.value;
+   // playBtn 누를 때마다 chances가 하나씩 까임
+   chances--;
+   chanceArea.textContent = `남은 기회: ${chances}번`;
+   console.log("chance", chances);
+
+   if(userValue < computerNum){
+        resultArea.textContent = "Up"
+   }else if(userValue > computerNum){
+        resultArea.textContent = "Down"
+   }else{
+        resultArea.textContent = "정답!"
+        // 숫자를 맞춘 후로는 더 이상 플레이 못하게
+        // if(gameOver == true){
+        //     playBtn.disabled = true;
+        // }
+   }
+
+   if(chances < 1){
+       gameOver = true
+   }
+   if(gameOver){
+       playBtn.disabled = true;
+   }
+}
+
+function reset(){
+    // user input창이 깨끗하게 정리되고
+    userInput.value = "";
+    // 새로운 번호가 생성되고
+    pickRandomNum();
+    resultArea.textContent = "결과"
+}
+
+function resetInput(){
+    userInput.value = "";
+}
+
+pickRandomNum();
